5c0cc5e2396ebd1e20dbd50b0ee9fb5e7b177dd3
Antonio Colaierà <colaiera@gmail.com>
2023-09-08 10:51:39 +0200

    first update

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,7 +4,7 @@ import { projects } from "../data";
 
 export default function Projects() {
   return (
-    <section id="progetti" className="text-gray-400  body-font">
+    <section id="progetti" className="text-gray-400 body-font">
       <div className="container px-5 py-10 mx-auto text-center lg:px-40">
         <div className="flex flex-col w-full mb-20">
           <CodeIcon className="mx-auto inline-block w-10 mb-4" />
@@ -13,28 +13,31 @@ export default function Projects() {
           </h1>
         </div>
 
-
-        
         <div className="flex flex-wrap -m-4">
           {projects.map((project) => (
             <a
               href={project.link}
               key={project.image}
-              className="sm:w-1/2 w-100 p-4">
+              className="sm:w-1/2 w-full p-4"
+            >
               <div className="flex relative">
                 <img
                   alt="gallery"
-                  className="absolute inset-0 w-full h-full object-cover object-center rounded-8 border-8 border-yellow-200"
+                  className="absolute inset-0 w-full h-full object-contain object-center rounded-8 border-8 border-yellow-200"
                   src={project.image}
                 />
-                <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-100">
-                  <h2 className="tracking-widest text-sm title-font font-medium text-green-400 mb-1">
+                <div className="px-4 sm:px-8 py-6 sm:py-10 relative z-10 w-full h-auto sm:h-64 border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-100">
+                  <h2 className="tracking-widest text-sm sm:text-xs title-font font-medium text-green-400 mb-1">
                     {project.subtitle}
                   </h2>
-                  <h1 className="title-font text-lg font-medium text-white mb-3">
+                  <h1 className="title-font text-lg sm:text-base font-medium text-white mb-3">
                     {project.title}
                   </h1>
-                  <p className="leading-relaxed">{project.description}</p>
+                  <div>
+                    <p className="leading-relaxed text-sm sm:text-xs">
+                      {project.description}
+                    </p>
+                  </div>
                 </div>
               </div>
             </a>
--- src/data.js
+++ src/data.js
@@ -1,11 +1,19 @@
 export const projects = [
   {
+    title: "Full-Stack-E-commerce-Cigar-and-Whiskey",
+    subtitle: "Sito web con Database",
+    description: "Questo sito web utilizza REACT, MySQL, SPRING BOOT e un'API REST, dispone di un carello per gli acquisti disponibile e aggiornato in tutte le pagine.",
+    image: "./E-commerce.gif",
+    link: "https://github.com/AntonioColaiera/Full-Stack-E-commerce-Cigar-and-Whiskey",
+  },
+
+  {
     title: "Castelli di Calabria",
     subtitle: "Sito web con Database",
-    description: "Questo sito web REACT usa le operazioni CRUD per recuperare informazioni da un Database MySQL  attraverso SPRING BOOT e un'API REST.",
+    description: "Questo sito web REACT usa le operazioni CRUD per recuperare informazioni da un Database MySQL attraverso SPRING BOOT e un'API REST.",
     image: "./CastelliCalabria.gif",
     link: "https://github.com/AntonioColaiera/Personal-Project-Castles-of-Calabria-WebSite",
-  },
+  }
 ];
 
 
